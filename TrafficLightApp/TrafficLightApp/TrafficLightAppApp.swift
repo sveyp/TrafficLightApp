55c7d7e5ee07504116abf24230797b19c62c394b
Svetlomir Petrov <svetlomir.petrov@scalefocus.com>
2025-03-18 23:50:59 +0200

    [TLA] Remove user-specific files from version control and update .gitignore

---
+++ TrafficLightApp/TrafficLightApp/TrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppApp.swift
+//  TrafficLightApp
+//
+//  Created by Svetlomir Petrov on 18.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrafficLightApp/TrafficLightAppUITests/TrafficLightAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TrafficLightAppUITestsLaunchTests.swift
+//  TrafficLightAppUITests
+//
+//  Created by Svetlomir Petrov on 18.03.25.
+//
+
+import XCTest
+
+final class TrafficLightAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
